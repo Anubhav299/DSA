3e26217d12c0974fac5a4e94b61a4e8c49307558
Anubhav299 <anubhav.linkedin77@gmail.com>
2024-08-17 20:24:00 +0530

    ifelse commit

--- step1/1.2/7.cpp
+++ step1/1.2/7.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    int n,i,j,k=1;
+    int n,i,j,space=2*(n-1);
 
     cout<<"Enter Value of n - ";
     cin>>n;
 
-    for(i)
+    for(i=1;i<)
 
     return 0;
 }

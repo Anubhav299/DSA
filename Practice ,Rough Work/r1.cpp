c9544c8324927337f5f7695a1517c3d504b99eaf
Anubhav299 <anubhav.linkedin77@gmail.com>
2025-04-24 14:47:11 +0530

    new

--- Practice ,Rough Work/r1.cpp
+++ Practice ,Rough Work/r1.cpp
@@ -1,32 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int numRabbits(vector<int> &answers)
-{
-    int ans = 0, cnt = 0;
-    unordered_map<int, int> mpp;
-    for (auto it : answers)
-    {
-        mpp[it]++;
-    }
-    for (auto it : mpp)
-    {
-        if (it.first == 0)
-        {
-            ans += it.second;
-        }
-        else
-        {
-            int grp = it.first + 1;
-            ans += ((grp)*(ceil((float)it.second/(grp))));
-        }
-    }
-    return ans;
-}
-
 int main()
 {
-    vector<int> arr = {};
-    cout << numRabbits(arr);
+    int Numbers[] = {2, 4, 8, 10};
+    int *ptr = Numbers;
+    for (int C = 0; C < 3; C++)
+    {
+        cout << *ptr << "@";
+        ptr++;
+    }
+    cout << endl;
+    for (int C = 0; C < 4; C++)
+    {
+        (*ptr) *= 2;
+        --ptr;
+    }
+    for (int C = 0; C < 4; C++)
+        cout << Numbers[C] << "#";
+    cout << endl;
     return 0;
 }
